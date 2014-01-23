7507877df08b052025e5ddbda423f7c759cb67f6
DesignMap Spare <designmap@DesignMaps-MacBook-Pro.local>
2014-01-22 16:34:10 -0800

    v5: isotope .item height, move info-panel js into main.js, breakpoint styles

--- healthagen-v2_project/build/js/main.js
+++ healthagen-v2_project/build/js/main.js
@@ -1,1 +1,42 @@
+// extend jQuery with a method to test if a jQuery-selector returns an empty object
+$.fn.exists = function () {
+    return this.length !== 0;
+}
+
+// info-panel
+jQuery(document).ready(function ($) {  
+  var $window = $(window),
+      $body = $('body'),
+      $html = $('html'),
+      $panel = $('#panel'),
+      $win = $('.win', $panel),
+      $width = $('.width', $panel),
+      $fontsize = $('.fontsize', $panel),
+      $breakpoint = $('.breakpoint', $panel);
+
+  function toggleBreakpoint() {
+    var windowWidth = $window.width();
+     // toggle breakpoint classes
+    $html.toggleClass('desktop', (windowWidth >= 1025));
+    $html.toggleClass('tablet', ( (windowWidth >= 768) && (windowWidth <= 1024) ));
+    $html.toggleClass('mobile', (windowWidth <= 767) );           
+  }
+
+  $win.html($window.width() + 'px');
+  $width.html($body.outerWidth() + 'px');
+  $fontsize.html($('html').css('font-size'));
+
+  toggleBreakpoint();
+
+  $(window).on('resize', function(){
+      //console.log($window.width());
+      $win.html($window.width() + 'px');
+      $width.html($body.outerWidth() + 'px');
+      $fontsize.html($('html').css('font-size'));
+
+      toggleBreakpoint();
+  });
+// end .ready        
+});
+
 
