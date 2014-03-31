60516234e62f2085172cca9cb254e5c9bdcba3a1
chad-schroeder <gmchad@gmail.com>
2014-03-31 09:41:54 -0700

    Mon Mar 31

--- angular/app/directives/holderFix.js
+++
@@ -1,7 +0,0 @@
-app.directive('holderFix', function () {
-    return {
-        link: function (scope, element, attrs) {
-            Holder.run({ images: element[0], nocss: true });
-        }
-    };
-});
